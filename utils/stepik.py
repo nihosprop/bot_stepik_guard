e5390c84a7cdf5164d2f4df235aa0abfaa35092e
тnihosprop <niwibonihs@tutamail.com>
2025-08-02 14:29:08 +0300

    refactor

--- utils/stepik.py
+++ utils/stepik.py
@@ -35,7 +35,7 @@ class StepikAPIClient:
         data = {
             'grant_type': 'client_credentials',
             'client_id': self.client_id,
-            'client_secret': self.client_secret, }
+            'client_secret': self.client_secret}
 
         try:
             async with (aiohttp.ClientSession() as session):
