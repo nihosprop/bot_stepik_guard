a55d734e34b123a40eff05e88e20db14690449ab
тnihosprop <niwibonihs@tutamail.com>
2025-08-04 16:39:56 +0300

    feat: add filter

--- filters/filters.py
+++ filters/filters.py
@@ -1,8 +1,12 @@
 import logging
 import json
 import os
+import re
+
 from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery, Message
+
+from utils.patterns import Patterns
 
 logger_filters = logging.getLogger(__name__)
 
@@ -17,3 +21,15 @@ class AccessRightsFilter(BaseFilter):
         user_tg_id = msg.from_user.id
         
         return user_tg_id in owners
+
+# Комбинированный фильтр
+def is_profanity(text: str) -> bool:
+    text = text.lower()
+    # Проверка основного паттерна
+    if Patterns.profanity_pattern.search(text):
+        return True
+    # Проверка дополнительных паттернов
+    for pattern in Patterns.additional_patterns:
+        if re.search(pattern, text, flags=re.IGNORECASE):
+            return True
+    return False
