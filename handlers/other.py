902c8fc37c239c61299f2e84411a5017e273952e
тnihosprop <niwibonihs@tutamail.com>
2025-08-05 03:33:05 +0300

    refactor

--- handlers/other.py
+++ handlers/other.py
@@ -1,12 +1,8 @@
 import logging
 
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, Message
 
 from filters.filters import AccessRightsFilter
-from utils.stepik import StepikAPIClient
-from utils.utils import MessageProcessor, get_username
-from keyboards.keyboards import kb_start
 
 logger_owners = logging.getLogger(__name__)
 
