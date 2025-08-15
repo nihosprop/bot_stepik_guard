4fed8f6f07c39364900b8ada24ff449ea24f8f05
тnihosprop <niwibonihs@tutamail.com>
2025-08-15 20:43:09 +0300

    feat(user_handlers): add AccessRightsFilter to message and callback query handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,13 @@ import logging
 
 from aiogram import Router
 
+from filters.filters import AccessRightsFilter
+
 logger = logging.getLogger(__name__)
 
 user_router = Router()
+
+user_router.message.filter(AccessRightsFilter(flag_users=True))
+user_router.callback_query.filter(AccessRightsFilter(flag_users=True))
+
 
