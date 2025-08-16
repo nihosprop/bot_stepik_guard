8cd050eca9778b1ecec8ae843411dfb90b2a267a
тnihosprop <niwibonihs@tutamail.com>
2025-08-16 20:49:56 +0300

    refactor
    Signed-off-by: тnihosprop <niwibonihs@tutamail.com>

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -1,6 +1,7 @@
 BUTTS_OWN_START: dict[str, str] = {
     'settings_courses': 'Курсы',
     'settings_users': 'Юзеры',
-    'user_settings': 'Настройки'}
+    'all_settings': 'Настройки'}
 
-BUTTS_USER_START: dict[str, str] = {'user_settings': 'Настройки'}
+BUTTS_USER_START: dict[str, str] = {'all_settings': 'Настройки'}
+
