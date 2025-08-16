0bebfad88075834d6c16c3a83c9b2d5164dda13c
тnihosprop <niwibonihs@tutamail.com>
2025-08-16 20:56:24 +0300

    feat: add buttons
    Signed-off-by: тnihosprop <niwibonihs@tutamail.com>

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -5,3 +5,10 @@ BUTTS_OWN_START: dict[str, str] = {
 
 BUTTS_USER_START: dict[str, str] = {'all_settings': 'Настройки'}
 
+BUTTS_SETTINGS_COURSES: dict[str, str] = {
+    'add_course': 'Добавить курс',
+    'delete_course': 'Удалить курс'}
+
+BUTTS_SETTINGS_USERS: dict[str, str] = {
+    'add_user': 'Добавить юзера',
+    'delete_user': 'Удалить юзера'}
