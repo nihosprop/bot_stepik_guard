264bde809fdfccba310461d69af2b249a28d0b08
тnihosprop <niwibonihs@tutamail.com>
2025-08-16 21:04:20 +0300

    feat: add imports
    Signed-off-by: тnihosprop <niwibonihs@tutamail.com>

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,6 +1,8 @@
 from keyboards.kb_utils import create_static_kb
-from keyboards.buttons import BUTTS_OWN_START
-
+from keyboards.buttons import (BUTTS_OWN_START,
+                               BUTTS_SETTINGS_COURSES,
+                               BUTTS_SETTINGS_USERS,
+                               BUTTS_USER_START)
 
 kb_own_start = create_static_kb(**BUTTS_OWN_START, cancel_butt=False)
 
