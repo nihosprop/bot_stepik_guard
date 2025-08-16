2249afc5bb3199c8787ff5b0b98e93b8fe081180
тnihosprop <niwibonihs@tutamail.com>
2025-08-16 21:04:34 +0300

    feat: add keyboards
    Signed-off-by: тnihosprop <niwibonihs@tutamail.com>

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,4 +5,8 @@ from keyboards.buttons import (BUTTS_OWN_START,
                                BUTTS_USER_START)
 
 kb_own_start = create_static_kb(**BUTTS_OWN_START, cancel_butt=False)
-
+kb_user_start = create_static_kb(**BUTTS_USER_START, cancel_butt=False)
+kb_settings_courses = create_static_kb(
+    **BUTTS_SETTINGS_COURSES,
+    cancel_butt=False)
+kb_settings_users = create_static_kb(**BUTTS_SETTINGS_USERS, cancel_butt=False)
