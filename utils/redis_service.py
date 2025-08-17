960e69eeb1dfb9936e012aa10f37d1b7665b9a0d
тnihosprop <niwibonihs@tutamail.com>
2025-08-18 01:02:42 +0300

    fix
    Signed-off-by: тnihosprop <niwibonihs@tutamail.com>

--- utils/redis_service.py
+++ utils/redis_service.py
@@ -93,7 +93,7 @@ class RedisService:
         """
         users_ids = await self.redis.smembers(self.users_list_set)
         if not users_ids:
-            return []
+            return ''
         
         str_users_ids = [str(user_id) for user_id in users_ids]
         keys = [f'{self.user_tag}:{user_id}' for user_id in str_users_ids]
