7f2c26c72fccbd8b90f73af8088b65fd16df42a1
nihosprop <niwibonihs@tutamail.com>
2025-08-21 13:07:03 +0300

    feat: add import RedisService
    Signed-off-by: nihosprop <niwibonihs@tutamail.com>

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -6,6 +6,7 @@ from aiogram.types import Message, CallbackQuery
 
 from filters.filters import AccessRightsFilter
 from keyboards.keyboards import kb_user_start, kb_own_start
+from utils.redis_service import RedisService
 from utils.utils import MessageProcessor, get_username
 
 logger = logging.getLogger(__name__)
