cc14321afc0caeb5c38c6900c111e12f5e0bdb4c
nihosprop <niwibonihs@tutamail.com>
2025-08-22 19:26:33 +0300

    refactor
    Signed-off-by: nihosprop <niwibonihs@tutamail.com>

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -104,8 +104,8 @@ async def cmd_start(msg: Message,
     await msg_processor.save_msg_id(value, msgs_for_del=True)
     
     if user_tg_id in owners:
-        await redis_service.add_owner(tg_user_id=user_tg_id,
-                                      tg_nickname=await get_username(msg))
+        await redis_service.add_owner(
+            tg_user_id=user_tg_id, tg_nickname=await get_username(msg))
 
     await state.set_state(None)
     logger.debug(f'State clear:{await get_username(msg)}:{msg.from_user.id}')
