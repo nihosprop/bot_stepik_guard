7fccabb7479c4c9161cf90bc5457f389d5af2f18
nihosprop <niwibonihs@tutamail.com>
2025-09-14 23:49:05 +0300

    refactor
    Signed-off-by: nihosprop <niwibonihs@tutamail.com>

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,9 +1,9 @@
 from keyboards.kb_utils import create_static_kb
-from keyboards.buttons import (BUTTS_OWN_START,
+from keyboards.buttons import (BUTTS_ALL_SETTINGS,
+                               BUTTS_OWN_START,
                                BUTTS_SETTINGS_COURSES,
                                BUTTS_SETTINGS_USERS,
-                               BUTTS_USER_START,
-                               BUTTS_ALL_SETTINGS)
+                               BUTTS_USER_START)
 
 kb_exit = create_static_kb(cancel_butt=False, exit_=True)
 
@@ -12,7 +12,6 @@ kb_user_start = create_static_kb(**BUTTS_USER_START, cancel_butt=False)
 
 kb_settings_users = create_static_kb(**BUTTS_SETTINGS_USERS, cancel_butt=True)
 kb_add_del_user = create_static_kb(back=True, cancel_butt=True)
-
 
 kb_settings_courses = create_static_kb(
     **BUTTS_SETTINGS_COURSES, cancel_butt=True)
