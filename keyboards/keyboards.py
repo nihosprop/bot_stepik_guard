5579a255f7ac007c139ed3213eb14c8994bdd85a
nihosprop <niwibonihs@tutamail.com>
2025-09-16 09:00:42 +0300

    refactor
    Signed-off-by: nihosprop <niwibonihs@tutamail.com>

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,10 +5,10 @@ from keyboards.buttons import (BUTTS_ALL_SETTINGS,
                                BUTTS_SETTINGS_USERS,
                                BUTTS_USER_START)
 
-kb_exit = create_static_kb(cancel_butt=False, exit_=True)
+kb_exit = create_static_kb(exit_=True)
 
-kb_own_start = create_static_kb(**BUTTS_OWN_START, cancel_butt=False)
-kb_user_start = create_static_kb(**BUTTS_USER_START, cancel_butt=False)
+kb_own_start = create_static_kb(**BUTTS_OWN_START)
+kb_user_start = create_static_kb(**BUTTS_USER_START)
 
 kb_settings_users = create_static_kb(**BUTTS_SETTINGS_USERS, cancel_butt=True)
 kb_add_del_user = create_static_kb(back=True, cancel_butt=True)
@@ -17,5 +17,4 @@ kb_settings_courses = create_static_kb(
     **BUTTS_SETTINGS_COURSES, cancel_butt=True)
 kb_add_del_course = create_static_kb(back=True, cancel_butt=True)
 
-kb_all_settings = create_static_kb(**BUTTS_ALL_SETTINGS,
-                                   cancel_butt=False, exit_=True)
+kb_all_settings = create_static_kb(**BUTTS_ALL_SETTINGS, exit_=True)
